e87bfe9afb3bad312574a25e4a18d78c5f3809bf
Y Paing <helloye@Ys-iMac.fios-router.home>
2019-04-16 23:30:20 -0400

    Updates

---
+++ precache-manifest.a8764b62bedeb18278ebdd2ff127f9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "bb5f25bdcdec7f80a0ef",
+    "url": "/static/js/main.b53335f9.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "bb5f25bdcdec7f80a0ef",
+    "url": "/static/css/main.7814f788.chunk.css"
+  },
+  {
+    "revision": "ddcf0e177dd1ade46872877b47701cae",
+    "url": "/index.html"
+  }
+];
